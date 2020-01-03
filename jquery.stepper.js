e1fb984bff0cc26b93c38583e445eb9bf7c0de9f
Saisiva123 <36258631+Saisiva123@users.noreply.github.com>
2020-01-03 09:22:02 -0500

    Add files via upload

--- jquery.stepper.js
+++ jquery.stepper.js
@@ -1,6 +1,6 @@
 (function ($) {
   $.fn.stepper = function (options) {
-    var x = [];
+
     var container = $(this);
     var settings = $.extend(
       {
@@ -24,7 +24,7 @@
 
     //set content,classes for lists and buttons
     for (i = 0; i < $count; i++) {
-      x[i] = 0;
+
       $stepperMain.append(createList(i));
     }
 
@@ -42,51 +42,65 @@
       $(".stepperList").eq(i).addClass("notvisited");
     }
     //for active class
-    $(".stepperList").eq(initialstep).addClass("current" +' '+ settings.stepperprocesscolor).removeClass("notvisited");
+    $(".stepperList").eq(initialstep).addClass("current" + ' ' + settings.stepperprocesscolor).removeClass("notvisited");
     $(".form").eq(initialstep).show();
     //initially first form and stepperList will be active
 
     $(".stepperList").click(function () {
       $num = $(this).index();
       console.log($num);
-      $(this).addClass("current").removeClass("isnotvisited");
+      
+      $(this).addClass("current").removeClass("isnotvisited").siblings().removeClass("current");
+      
       if (($(this).hasClass("current") && $(this).prev().hasClass('visited')) || $(this).hasClass("visited")) {
         $(".form").hide();
         $(".form").eq($num).show();
       }
     });
 
-    $(".pageButtons").eq(0).click(function()
-    {
-        $index=$(".current").index();
-        console.log($index);
-        if($(".form").eq($index))
-        {
-         // $index=$(this).index();
+    $(".pageButtons").eq(0).click(function () {
+      $(".form").each(function () {
+        if ($(this).css("display") == "block") {
+          if ($(this).prevAll().length - 1 > 0) {
+            $(this).fadeOut().prev().fadeIn();
+          }
         }
+      });
+    });
+
+    $(".pageButtons").eq(1).click(function () {
+      $(".stepperList").each(function () {
+        if ($(this).hasClass("current")) {
+          $index=$(this).index();
+          console.log($index);
+          if ($(".form").eq($index+1).nextAll().length -1> 0) {
+            
+            $(".form").eq($index).fadeOut("fast").next().fadeIn("slow");
+          }
+        }
+      });
     });
 
     //when the buttons clicked
 
     buttons.click(function () {
       var buttonclick = $(this);
-      $index = buttonclick 
+      $index = buttonclick
         .parents(".form")
         .index();
-    
+
       if ($index < $count) {
-     $(".form").eq($index-1)
-         .hide();
-     $(".stepperList").eq($index-1).addClass("visited").removeClass("notvisited current");
-     $(".form").eq($index)
+        $(".form").eq($index - 1)
+          .hide();
+        $(".stepperList").eq($index - 1).addClass("visited").removeClass("notvisited current");
+        $(".form").eq($index)
           .fadeIn('slow');
-     if($(".stepperList").eq($index).hasClass("notvisited"))
-     {
-      $(".stepperList").eq($index).addClass("current"+' '+ settings.stepperprocesscolor).removeClass("notvisited");
-     }
+        if ($(".stepperList").eq($index).hasClass("notvisited")) {
+          $(".stepperList").eq($index).addClass("current" + ' ' + settings.stepperprocesscolor).removeClass("notvisited");
+        }
       }
       $(".stepperList:nth-child(" + $index + ")").removeClass(settings.stepperprocesscolor);
-      $(".stepperList:nth-child(" + $index + ")").addClass(settings.steppercompletioncolor );
+      $(".stepperList:nth-child(" + $index + ")").addClass(settings.steppercompletioncolor);
     });
   };
 })(jQuery);
